fe219de0f4511bbc5881cf32ac5e55d5242d7492
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-02-07 08:28:22 +0500

    DeleteUserMenuOption.java

--- src/menu/options/DeleteUserMenuOption.java
+++ src/menu/options/DeleteUserMenuOption.java
@@ -1,4 +1,28 @@
 package menu.options;
 
-public class DeleteUserMenuOption {
+import menu.MenuOption;
+import controllers.UserController;
+import java.util.Scanner;
+
+public class DeleteUserMenuOption implements MenuOption {
+    private UserController userController;
+
+    public DeleteUserMenuOption(UserController userController) {
+        this.userController = userController;
+    }
+
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the user name to delete: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter the user surname: ");
+        String surname = scanner.nextLine();
+        userController.deleteUser(name, surname);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Delete a user";
+    }
 }
