e79f648efbc056c25f7e1b89144b6e735d4a2d55
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-02-07 08:24:39 +0500

    Update UserController.java

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -1,4 +1,43 @@
 package controllers;
 
+import models.User;
+import repositories.UserRepository;
+import java.sql.SQLException;
+import java.util.List;
+
 public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void showAllUsers() {
+        try {
+            List<User> users = userRepository.getAllUsers();
+            for (User user : users) {
+                System.out.println(user);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while fetching users: " + e.getMessage());
+        }
+    }
+
+    public void addUser(String name, String surname, String bookTitle) {
+        try {
+            userRepository.addUser(name, surname, bookTitle);
+            System.out.println("User added successfully!");
+        } catch (SQLException e) {
+            System.out.println("Error while adding user: " + e.getMessage());
+        }
+    }
+
+    public void deleteUser(String name, String surname) {
+        try {
+            userRepository.deleteUser(name, surname); // Now this method exists in UserRepository
+            System.out.println("User deleted successfully!");
+        } catch (SQLException e) {
+            System.out.println("Error while deleting user: " + e.getMessage());
+        }
+    }
 }
