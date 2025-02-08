1f26165dbdffa9e56ce6560db5851eaccc61228b
yernar0 <ernarorleu9988@gmail.com>
2025-02-08 20:40:53 +0500

    menu Menu.java

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,4 +1,25 @@
 package menu;
 
+import java.util.List;
+
 public class Menu {
+    private List<MenuOption> menuOptions;
+
+    public Menu(List<MenuOption> menuOptions) {
+        this.menuOptions = menuOptions;
+    }
+
+    public void display() {
+        for (int i = 0; i < menuOptions.size(); i++) {
+            System.out.println((i + 1) + ". " + menuOptions.get(i).getDescription());
+        }
+    }
+
+    public void executeOption(int optionNumber) {
+        if (optionNumber >= 1 && optionNumber <= menuOptions.size()) {
+            menuOptions.get(optionNumber - 1).execute();
+        } else {
+            System.out.println("Invalid option. Please try again.");
+        }
+    }
 }
