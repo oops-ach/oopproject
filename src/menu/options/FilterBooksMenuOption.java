6502bcf66ea01ebe2d3f689f7708fde55cf1f03c
yernar0 <ernarorleu9988@gmail.com>
2025-02-08 20:38:15 +0500

    Update FilterBooksMenuOption.java

--- src/menu/options/FilterBooksMenuOption.java
+++ src/menu/options/FilterBooksMenuOption.java
@@ -1,4 +1,27 @@
 package menu.options;
 
-public class FilterBooksMenuOption {
+import menu.MenuOption;
+import controllers.BookController;
+import java.util.Scanner;
+
+public class FilterBooksMenuOption implements MenuOption {
+    private BookController bookController;
+
+    public FilterBooksMenuOption(BookController bookController) {
+        this.bookController = bookController;
+    }
+
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter status (available/taken): ");
+        String status = scanner.nextLine();
+
+        bookController.filterBooksByStatus(status);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Filter books by status (available/taken)";
+    }
 }
