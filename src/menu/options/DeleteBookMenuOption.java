c60c46fdb49f6e5037d8410b97ba6293bc9584df
danial41-design <121389931+danial41-design@users.noreply.github.com>
2025-02-10 15:00:50 +0500

    Update DeleteBookMenuOption.java

--- src/menu/options/DeleteBookMenuOption.java
+++ src/menu/options/DeleteBookMenuOption.java
@@ -1,4 +1,38 @@
 package menu.options;
 
-public class DeleteBookMenuOption {
+import menu.MenuOption;
+import controllers.BookController;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class DeleteBookMenuOption implements MenuOption {
+    private BookController bookController;
+
+    public DeleteBookMenuOption(BookController bookController) {
+        this.bookController = bookController;
+    }
+
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the book title to delete: ");
+        String title = scanner.nextLine();
+
+        try {
+            if (bookController.isBookAvailable(title)) {
+                bookController.deleteBook(title);  
+                System.out.println("Book deleted successfully.");
+            } else {
+                System.out.println("This book does not exist in the database.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while interacting with the database: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "Delete a book";
+    }
 }
