d6b4a6fa9f9631c2a2529cdea4f991b19a1fde63
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-02-11 11:29:29 +0500

    IDB.java

--- src/data/interfaces/IDB.java
+++ src/data/interfaces/IDB.java
@@ -1,4 +1,10 @@
-package data;
+
+package data.interfaces;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public interface IDB {
+    Connection getConnection() throws SQLException;
+    void close();
 }
