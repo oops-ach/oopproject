5170c034a5c29a561f999b72a5971c2ea4ada8f6
yernar0 <ernarorleu9988@gmail.com>
2025-02-11 11:23:22 +0500

    Update PostgreDB.java

--- src/data/PostgreDB.java
+++ src/data/PostgreDB.java
@@ -1,4 +1,39 @@
 package data;
 
-public class PostgreDB {
+import data.interfaces.IDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreDB implements IDB {
+    private String url;
+    private String user;
+    private String password;
+    private Connection connection;
+
+    public PostgreDB(String url, String user, String password, String database) {
+        this.url = url + "/" + database;
+        this.user = user;
+        this.password = password;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        return connection;
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
