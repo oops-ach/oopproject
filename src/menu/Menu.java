4da29ab1ed3cee6602a0f865c398573cc3494f74
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-02-14 08:57:30 +0500

    Menu.java

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,1 +1,26 @@
+package menu;
 
+import java.util.List;
+
+public class Menu {
+	private  List<MenuOption> menuOptions;
+
+	public Menu(List<MenuOption> menuOptions) {
+		this.menuOptions = menuOptions;
+	}
+
+	public void display() {
+		for (int i = 0; i < menuOptions.size(); i++){
+			System.out.println((i+1) + ". " + menuOptions.get(i).getDescription());
+		}
+	}
+
+	public void executeOption( int optionNumber){
+		if (optionNumber >= 1 && optionNumber <= menuOptions.size()){
+			menuOptions.get(optionNumber - 1).execute();
+		} else {
+			System.out.println("Invalid option number");
+		}
+	}
+
+}
