86f68e34dd0c62a83ff3764f2e5eff8ba98d45b4
danial41-design <121389931+danial41-design@users.noreply.github.com>
2025-02-14 09:08:05 +0500

    Update UserService.java

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,1 +1,27 @@
+package services;
+
+import models.User;
+import repositories.interfaces.IUserRepository;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserService {
+	private final IUserRepository userRepository;
+
+	public UserService(IUserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public List<User> getAllUsers() throws SQLException {
+		return userRepository.getAllUsers();
+	}
+
+	public void addUser(String name, String surname, String bookTitle) throws SQLException {
+		userRepository.addUser(name, surname, bookTitle);
+	}
+
+	public void deleteUser(String name, String surname) throws SQLException {
+		userRepository.deleteUser(name, surname);
+	}
+}
 
