98ba8e7f75e2b8f89f088739e10d9a24cbc9a704
danial41-design <121389931+danial41-design@users.noreply.github.com>
2025-02-14 09:04:24 +0500

    Update EmployeeRepository.java

--- src/repositories/EmployeeRepository.java
+++ src/repositories/EmployeeRepository.java
@@ -1,1 +1,80 @@
+package repositories;
 
+import repositories.interfaces.IEmployeeRepository;
+import models.*;
+import data.interfaces.IDB;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeRepository implements IEmployeeRepository {
+	private final IDB db;
+
+	public EmployeeRepository(IDB db) {
+		this.db = db;
+	}
+
+	@Override
+	public void addEmployee(String name, String surname, String position, double salary) throws SQLException {
+		String query = "INSERT INTO employee (name, surname, position, salary) VALUES (?, ?, ?, ?)";
+		try (Connection conn = db.getConnection();
+			 PreparedStatement pstmt = conn.prepareStatement(query)) {
+			pstmt.setString(1, name);
+			pstmt.setString(2, surname);
+			pstmt.setString(3, position);
+			pstmt.setDouble(4, salary);
+			pstmt.executeUpdate();
+		}
+	}
+
+	@Override
+	public void deleteEmployee(int employeeId) throws SQLException {
+		String query = "DELETE FROM employee WHERE id = ?";
+		try (Connection conn = db.getConnection();
+			 PreparedStatement pstmt = conn.prepareStatement(query)) {
+			pstmt.setInt(1, employeeId);
+			pstmt.executeUpdate();
+		}
+	}
+
+	@Override
+	public List<Employee> getEmployeesByPosition(String position) throws SQLException {
+		List<Employee> employees = new ArrayList<>();
+		String query = "SELECT * FROM employee WHERE position = ?";
+		try (Connection conn = db.getConnection();
+			 PreparedStatement pstmt = conn.prepareStatement(query)) {
+			pstmt.setString(1, position);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				employees.add(new Employee(
+						rs.getInt("id"),
+						rs.getString("name"),
+						rs.getString("surname"),
+						rs.getString("position"),
+						rs.getDouble("salary")
+				));
+			}
+		}
+		return employees;
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() throws SQLException {
+		List<Employee> employees = new ArrayList<>();
+		String query = "SELECT * FROM employee";
+		try (Connection conn = db.getConnection();
+			 Statement stmt = conn.createStatement();
+			 ResultSet rs = stmt.executeQuery(query)) {
+			while (rs.next()) {
+				employees.add(new Employee(
+						rs.getInt("id"),
+						rs.getString("name"),
+						rs.getString("surname"),
+						rs.getString("position"),
+						rs.getDouble("salary")
+				));
+			}
+		}
+		return employees;
+	}
+}
