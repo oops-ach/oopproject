791e23e690df9207a799034086e2b8b361356a90
kanaryth <rashirbek123.kz@gmail.com>
2025-02-14 09:03:51 +0500

    Update ShowAllEmployeesMenuOption.java

--- src/menu/options/ShowAllEmployeesMenuOption.java
+++ src/menu/options/ShowAllEmployeesMenuOption.java
@@ -1,1 +1,23 @@
+package menu.options;
+
+import menu.MenuOption;
+import controllers.EmployeeController;
+
+public class ShowAllEmployeesMenuOption implements MenuOption {
+	private final EmployeeController employeeController;
+
+	public ShowAllEmployeesMenuOption(EmployeeController employeeController) {
+		this.employeeController = employeeController;
+	}
+
+	@Override
+	public void execute() {
+		employeeController.showAllEmployees();
+	}
+
+	@Override
+	public String getDescription() {
+		return "Show all employees";
+	}
+}
 
