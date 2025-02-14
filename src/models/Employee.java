3150a0ebb27d8bd021c5bc36d4ab12f95e4c4c04
danial41-design <121389931+danial41-design@users.noreply.github.com>
2025-02-14 08:59:12 +0500

    Update Employee.java

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -1,1 +1,57 @@
+package models;
 
+public class Employee {
+	protected int id;
+	protected String name;
+	protected String surname;
+	protected String position;
+	protected double salary;
+
+	public Employee(int id, String name, String surname, String position, double salary) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.position = position;
+		this.salary = salary;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+
+	public void work(){
+		System.out.println(name + " is working as "+position);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", surname='" + surname + '\'' +
+				", position='" + position + '\'' +
+				", salary=" + salary +
+				'}';
+	}
+}
