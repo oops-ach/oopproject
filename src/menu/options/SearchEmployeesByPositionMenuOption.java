b9b862c41f0dfb3b5e85537dd4f3c1c0c2833ff2
kanaryth <rashirbek123.kz@gmail.com>
2025-02-14 09:03:07 +0500

    Update SearchEmployeesByPositionMenuOption.java

--- src/menu/options/SearchEmployeesByPositionMenuOption.java
+++ src/menu/options/SearchEmployeesByPositionMenuOption.java
@@ -1,1 +1,28 @@
+package menu.options;
+
+import menu.MenuOption;
+import controllers.EmployeeController;
+import java.util.Scanner;
+
+public class SearchEmployeesByPositionMenuOption implements MenuOption {
+	private final EmployeeController employeeController;
+
+	public SearchEmployeesByPositionMenuOption(EmployeeController employeeController) {
+		this.employeeController = employeeController;
+	}
+
+	@Override
+	public void execute() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter position to search (Manager/Librarian/Technician): ");
+		String position = scanner.nextLine();
+
+		employeeController.searchEmployeesByPosition(position);
+	}
+
+	@Override
+	public String getDescription() {
+		return "Search employees by position";
+	}
+}
 
