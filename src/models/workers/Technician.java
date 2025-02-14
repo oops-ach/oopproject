5e1f51fee75ea6b8aab676bb4ba8e5d39ded52de
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-02-14 09:05:55 +0500

    Technician.java

--- src/models/workers/Technician.java
+++ src/models/workers/Technician.java
@@ -1,14 +1,1 @@
-package models.workers;
 
-import models.Employee;
-
-public class Technician extends Employee {
-	public Technician(int id, String name, String surname, double salary) {
-		super(id, name, surname, "Technician", salary);
-	}
-
-	@Override
-	public void work() {
-		System.out.println(name + " is maintaining and repairing library equipment.");
-	}
-}
