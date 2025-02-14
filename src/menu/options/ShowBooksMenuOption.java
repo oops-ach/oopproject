0b5934b2cff636a9596fe27399644884c762d174
kanaryth <rashirbek123.kz@gmail.com>
2025-02-14 09:04:20 +0500

    Update ShowBooksMenuOption.java

--- src/menu/options/ShowBooksMenuOption.java
+++ src/menu/options/ShowBooksMenuOption.java
@@ -1,1 +1,23 @@
+package menu.options;
+
+import menu.MenuOption;
+import controllers.BookController;
+
+public class ShowBooksMenuOption implements MenuOption {
+	private final BookController bookController;
+
+	public ShowBooksMenuOption(BookController bookController) {
+		this.bookController = bookController;
+	}
+
+	@Override
+	public void execute() {
+		bookController.showAllBooks();
+	}
+
+	@Override
+	public String getDescription() {
+		return "Show all books";
+	}
+}
 
